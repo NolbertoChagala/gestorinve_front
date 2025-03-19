2d4dd5f2323926f5f5902058693330eeb8960405
Anthony <anthowill1608@gmail.com>
2025-03-19 14:58:17 -0500

    Se modificó el generic request

--- src/util/genericRequest.ts
+++ src/util/genericRequest.ts
@@ -1,19 +1,22 @@
-// genericRequest.ts
-import { apiClient } from '../services/ApiService'
+import api from '@/services/ApiService'
+import axios from 'axios'
 
 type HTTPMethod = 'GET' | 'POST' | 'PUT' | 'DELETE'
+const API_BASE_URL = 'https://localhost:7126/api'
 
 export const genericRequest = async (
   url: string,
   method: HTTPMethod,
   body?: any,
-  headers: Record<string, string> = {}
 ) => {
   try {
-    const response = await apiClient({
+    const response = await axios({
+      baseURL: API_BASE_URL,
       url,
       method,
-      headers: { ...apiClient.defaults.headers, ...headers },
+      headers: {
+        'Content-Type': 'application/json',
+      },
       data: body,
     })
     return response.data
@@ -21,6 +24,27 @@ export const genericRequest = async (
     const errorMessage = error.response?.data || error.message || 'Error desconocido'
     console.error(`Error en ${method} ${url}: ${errorMessage}`)
 
-    throw new Error(errorMessage)
+    return { error: true, message: error.response?.data || "Error desconocido" };
+  }
+}
+
+// Peticiones con autenticación
+export const genericRequestAuthenticated = async (
+  url: string,
+  method: HTTPMethod,
+  body?: any
+) => {
+  try {
+    const response = await api({
+      url: url,
+      method,
+      data: body
+    })
+    return response.data;
+  } catch (error: any) {
+    const errorMessage = error.response?.data || error.message || 'Error desconocido'
+    console.error(`Error en ${method} ${url}: ${errorMessage}`)
+
+    return { error: true, message: error.response?.data || "Error desconocido" };
   }
 }

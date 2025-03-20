35512fa41933e6420bfc747e95ff32812963fa01
Anthony <anthowill1608@gmail.com>
2025-03-20 13:45:52 -0500

    Se creó el modal para agregar categoría

---
+++ src/components/Modals/AddCategoryModal.vue
@@ -0,0 +1,34 @@
+<template>
+    <BaseModal 
+      :isOpen="isOpen" 
+      title="Crear Categoría" 
+      confirmButtonText="Guardar"
+      @close="closeModal"
+      @confirm="handleCreateCategory"
+    >
+      <input v-model="newCategory" type="text" placeholder="Nombre de la categoría" class="border p-2 w-full rounded">
+    </BaseModal>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref, defineProps, defineEmits } from 'vue';
+  import BaseModal from '@/components/BaseModal.vue';
+  
+  const props = defineProps({
+    isOpen: Boolean, // Control de visibilidad
+  });
+  
+  const emit = defineEmits(['close', 'confirm']);
+  
+  const newCategory = ref('');
+  
+  // Métodos
+  const closeModal = () => emit('close');
+  
+  const handleCreateCategory = () => {
+    console.log("Categoría creada:", newCategory.value);
+    emit('confirm', newCategory.value);
+    closeModal();
+  };
+  </script>
+  

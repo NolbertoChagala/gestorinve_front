981136b225acb70492a18a8dade4f5e5a6d919be
Anthony <anthowill1608@gmail.com>
2025-03-21 13:22:38 -0500

    Se creó el componente ConfirmDelete

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config';
 import ToastService from "primevue/toastservice";
+import Lara from '@primeuix/themes/lara';
+import ConfirmationService from 'primevue/confirmationservice';
+import ConfirmDialog from 'primevue/confirmdialog';
 
 import App from './App.vue'
 import router from './router'
@@ -12,7 +15,16 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue);
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+        options: {
+            darkModeSelector: false || 'none',
+        },
+    }
+});
+app.use(ConfirmationService);
+app.component('ConfirmDialog', ConfirmDialog);
 app.use(ToastService);
 
 app.mount('#app')
